5385899bf561fe1a58fac42b0a1545ee0dd88a4f
Andrei Huk <belakolala1@gmail.com>
2022-04-12 01:09:27 +0300

    Добавьте файлы проекта.

---
+++ Task_15_Console/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WARN_WIN32_FILE_EXISTS = unchecked((int)0x80070050);
+            string patch = @"D:\tes33t";
+            DirectoryInfo dirInfo = new DirectoryInfo(patch);
+
+            string mask = "*.TXT";
+
+            try
+            {
+                // Ищем файлы в корневой директории.
+                getFiles(dirInfo);
+                // Рекурсивный поиск по поддиректориям.
+                getChildDirectories(dirInfo);
+                Console.WriteLine("DONE");
+            }
+
+            catch (IOException ex) when (ex.HResult == WARN_WIN32_FILE_EXISTS)
+            {
+            }
+            
+
+
+
+
+
+            void getChildDirectories(DirectoryInfo rootDirectory)
+            {
+                foreach (DirectoryInfo directory in rootDirectory.GetDirectories())
+                {
+                    try
+                    {
+                        getFiles(directory);
+                        getChildDirectories(directory);
+                    }
+                    catch (IOException ex) when (ex.HResult == WARN_WIN32_FILE_EXISTS)
+                    {
+                    }
+                }
+            }
+            void getFiles(DirectoryInfo directory)
+            {
+                var files = Directory.EnumerateFiles(directory.FullName, mask, SearchOption.TopDirectoryOnly);
+                foreach (var file in files)
+                {
+                    //ZipFile.CreateFromDirectory(directory.FullName, file.Replace(".TXT", ".zip"), CompressionLevel.Fastest, true);
+                    ZipFile.CreateFromDirectory(directory.FullName, file.Replace(mask.Trim('*'), ".zip"), CompressionLevel.Fastest, true);
+
+                }
+            }
+        }
+
+    }
+}
